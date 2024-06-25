8f404b9a1070f0018ddcf72fd3b03998053bc0b3
semmlerl <76000537+semmlerl@users.noreply.github.com>
2024-06-25 11:43:55 +0200

    Added exp decay

--- src/data_import/class_tempcurve.py
+++ src/data_import/class_tempcurve.py
@@ -1,5 +1,7 @@
 import numpy as np
 from plotnine import ggplot, aes, geom_line, ggsave, geom_point
+import scipy.optimize
+import matplotlib.pyplot as plt
 
 
 class class_tempcurve: 
@@ -37,6 +39,7 @@ class class_tempcurve:
         
         self.features['mean_temp'] = self.cutted_trace['Temperature'].mean()
         self.features['min_temp'] = self.cutted_trace['Temperature'].min()
+        self.features['min_temp_t'] = np.argmin(self.cutted_trace['Temperature'])
         self.features['max_temp'] = self.cutted_trace['Temperature'].max()
         self.features['std_temp'] = self.cutted_trace['Temperature'].std()
         self.features['length'] = self.cutted_trace['Temperature'].shape[0]
@@ -67,6 +70,26 @@ class class_tempcurve:
         self.features['cooling_energy'] = sum(x - self.features['max_temp'] for x in self.cutted_trace['Temperature'])
         self.features['average_cooling_energy'] = self.features['cooling_energy']/ self.features['length']
         
+        ## calculate the exponential decay fit  
+        ## using bounds to set the starting point of the fit to the temperature at time 0 and the limit to the min temperature, so only the tau is actually fitted 
+        params, cv = scipy.optimize.curve_fit(exp_decay, 
+                                              self.cutted_trace['Time'].iloc[0:self.features['min_temp_t']], 
+                                              self.cutted_trace['Temperature'].iloc[0:self.features['min_temp_t']],
+                                              bounds = (
+                                                  [((self.cutted_trace['Temperature'].iloc[0]-1) + abs((self.features['min_temp']-1))), -100, (self.features['min_temp']-1)], 
+                                                  [(self.cutted_trace['Temperature'].iloc[0] + abs((self.features['min_temp']-1))), 100, self.features['min_temp']]
+                                                    )
+                                              )
+        self.features['tau_decay'] = params[1]
+        
+        """ plotting the decay fit 
+        a, tau, min_temp = params 
+        plt.plot( self.cutted_trace['Time'].iloc[0:self.features['min_temp_t']], self.cutted_trace['Temperature'].iloc[0:self.features['min_temp_t']], '.', label="data")
+        plt.plot(self.cutted_trace['Time'].iloc[0:self.features['min_temp_t']], exp_decay(self.cutted_trace['Time'].iloc[0:self.features['min_temp_t']], a, tau, min_temp), '--', label="fitted")
+        plt.title("Fitted Exponential Curve" + str(params))
+        plt.show()
+        """        
+        
     def plot_tempcurve(self, outpath): 
         
         g = (
@@ -76,5 +99,12 @@ class class_tempcurve:
         )
         
         ggsave(g, filename = (outpath + self.trace_id + ".png"))
-        print(outpath + self.trace_id + ".png saved")        
+        print(outpath + self.trace_id + ".png saved")  
+        
+        
+        
+def exp_decay(x, a, tau, min_temp):
+    ## returns an exponential decay function to model the temp decay 
+    
+    return a*np.exp(-tau * x) + min_temp
         
