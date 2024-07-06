6bf4a02680a0cb106bfe6be86a98f5b07b1f0c68
semmlerl <76000537+semmlerl@users.noreply.github.com>
2024-07-06 12:55:02 +0200

    added model raw_data

--- src/models/neural_net_raw_data_V0.py
+++ src/models/neural_net_raw_data_V0.py
@@ -4,26 +4,30 @@ import pickle
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Input, Dropout
+from tensorflow.keras.layers import Dense, Input, Dropout, Conv1D, MaxPooling1D, Flatten
 from sklearn.metrics import confusion_matrix,ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 
 def main(): 
 
-    with open("../../../../data/extracted/extracted_features_df.p", 'rb') as f: data = pickle.load(f)
-    
+    with open("../../../../data/extracted/extracted_raw_data_df.p", 'rb') as f: data = pickle.load(f)
      
     ##splitting into features and labels 
-    features_df = data.iloc[:,0:18]
+    features_df = data.iloc[:,13:]
     labels_df = data['Recurrence']
     
-    # normalizing features data using min_max_normalizer 
-    for column in features_df.columns:
-        features_df[column] = (features_df[column] - min(features_df[column]))/(max(features_df[column]) - min(features_df[column]))
-    
     # handling nan values
-    features_df = features_df.fillna(1)   
-    features_df.isna().sum()  
+    features_df = features_df.fillna(40)   
+    features_df.isna().sum()
+    
+    # normalizing features data using min_max_normalizer 
+    features_df = (features_df + 60)/100
+    
+    ## replacing all values > 1 with 1
+    features_df = features_df.applymap(lambda x: 1 if x > 1 else x)
+    
+    ## replacing all values <0 with 0
+    features_df = features_df.applymap(lambda x: 0 if x < 0 else x)    
     
     # Splitting the data into training and test sets
     X_train, X_test, y_train, y_test = train_test_split(features_df, labels_df, test_size=0.2, random_state=None)
@@ -36,10 +40,13 @@ def main():
   
     # Define the neural network model
     model = Sequential([
-        Input(shape =(X_train.shape[1],)),       
-        Dense(64, activation='relu'),
-        Dropout(0.5),
-        Dense(64, activation='relu'),
+        Input(shape =(X_train.shape[1],1)),
+        Conv1D(filters=32, kernel_size=3, activation='relu'),
+        MaxPooling1D(pool_size=2),
+        Flatten(), 
+        Dense(256, activation='relu'),
+        Dropout(0.5), 
+        Dense(256, activation='relu'),
         Dense(1, activation='sigmoid')
     ])    
     
@@ -60,10 +67,10 @@ def main():
     
     print(f"Test accuracy: {test_acc}")
     
-    plot_confusion_matrix(y_pred_prob, y_test)
+    plot_confusion_matrix(y_pred_prob, y_test, cutoff = 0.3)
     
-    ## generates a histogramm for each of the labels with predicted values
     plot = pd.DataFrame({'pred': y_pred_prob, 'true': y_test})
+    
     plot.groupby('true')['pred'].hist(alpha = 0.5, legend = True)
     
 if __name__ == "__main__":
